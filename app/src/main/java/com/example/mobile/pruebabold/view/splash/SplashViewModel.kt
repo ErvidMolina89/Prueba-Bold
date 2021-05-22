0fe0d3146a4ec835cec264cb51c9be719584ce82
eralmoba <eralmoba@hotmail.com>
2021-05-21 22:45:55 -0500

    First commit estructure

---
+++ app/src/main/java/com/example/mobile/pruebabold/view/splash/SplashViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.mobile.pruebabold.view.splash
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SplashViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
